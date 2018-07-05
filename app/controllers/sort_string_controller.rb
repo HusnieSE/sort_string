dfe463d171701fe2af5fa8362fcc75b61a257505
HusnieSE <husnie.edres@gmail.com>
2018-07-05 21:05:32 +0800

    added something

--- app/controllers/sort_string_controller.rb
+++ app/controllers/sort_string_controller.rb
@@ -2,11 +2,10 @@ class SortStringController < ApplicationController
   def index
     input_string = params[:text_string]
     type = params[:sort_type]
-    @text = []
     if type == "bubble_sort"
-      input_string.nil? ? "" :@text << {"sorted": SortStringModel.bubbleSort(input_string), "input_string": params[:text_string]}
+      input_string.nil? ? "" : @text = SortStringModel.bubbleSort(input_string)
     else
-      input_string.nil? ? "" :@text << {"sorted": SortStringModel.quickSort(input_string, 0, nil), "input_string": params[:text_string]}
+      input_string.nil? ? "" : @text = SortStringModel.quickSort(input_string, 0, nil)
     end
   end
 
